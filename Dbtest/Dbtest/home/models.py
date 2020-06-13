83ef3a25ef40357010351255ae2d9633a4178e65
Ravin <ravinrakholiya559@gmail.com>
2020-06-13 18:07:12 +0530

    updated pp and tc

--- Dbtest/Dbtest/home/models.py
+++ Dbtest/Dbtest/home/models.py
@@ -3,19 +3,19 @@ from django.db import models
 # Create your models here.
 
 class tbl2Consultants(models.Model):
-    id = models.AutoField
+    # id = models.AutoField
     name=models.CharField(max_length=500, default='')
 
 class tbl2HealthCenters(models.Model):
-    id = models.AutoField
+    # id = models.AutoField
     name=models.CharField(max_length=100, default='')
 
 class tbl2Patients(models.Model):
-    id = models.AutoField
+    # id = models.AutoField
     name=models.CharField(max_length=500, default='')
 
 class tbl2Cases(models.Model):
-    id = models.AutoField
+    # id = models.AutoField
     CaseNumber = models.CharField(max_length=50, default='')
     PatientID = models.ForeignKey(tbl2Patients,default=None,null=True,on_delete=models.SET_NULL)
     HealthCenterID = models.ForeignKey(tbl2HealthCenters,default=None,null=True,on_delete=models.SET_NULL)
